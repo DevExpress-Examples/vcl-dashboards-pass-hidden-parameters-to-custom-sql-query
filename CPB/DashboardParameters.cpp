bbff1983e95a260315d1d31e20a752c627612011
Nick Volynkin <nikolay.volynkin@devexpress.com>
2026-02-19 20:34:07 +0200

    cpp-builder: Set application title and icon

--- CPB/DashboardParameters.cpp
+++ CPB/DashboardParameters.cpp
@@ -12,6 +12,7 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 	{
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
+		Application->Title = "DevExpress Dashboards Example";
 		Application->CreateForm(__classid(TMainForm), &MainForm);
 		Application->Run();
 	}
