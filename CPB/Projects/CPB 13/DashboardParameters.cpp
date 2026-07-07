5cb902c92b9f6f4338e5015a707735038c83802e
Ilia Nenashev <ilia.nenashev@devexpress.com>
2026-07-07 16:39:50 +0300

    Split CPB by versions of runtime packages

--- CPB/Projects/CPB 13/DashboardParameters.cpp
+++ CPB/Projects/CPB 13/DashboardParameters.cpp
@@ -4,7 +4,7 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("uMainForm.cpp", MainForm);
+USEFORM("..\..\Sources\uMainForm.cpp", MainForm);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
